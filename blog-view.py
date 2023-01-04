cde95a4d7a67a33ee419b81334dbd2808678e497
johnjohn34kj2 <121136757+johnjohn34kj2@users.noreply.github.com>
2023-01-04 23:33:33 +0200

    Update blog-view.py

--- blog-view.py
+++ blog-view.py
@@ -7,16 +7,6 @@ headers = {
 	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
 }
 
-     _____ ___  ____            ____  _              __     ___                     ____        _                                                     
-    |  ___/ _ \/ ___|          | __ )| | ___   __ _  \ \   / (_) _____      _____  | __ )  ___ | |_                                                   
-    | |_ | | | \___ \   _____  |  _ \| |/ _ \ / _` |  \ \ / /| |/ _ \ \ /\ / / __| |  _ \ / _ \| __|                                                  
-    |  _|| |_| |___) | |_____| | |_) | | (_) | (_| |   \ V / | |  __/\ V  V /\__ \ | |_) | (_) | |_                                                   
-    |_|   \___/|____/          |____/|_|\___/ \__, |    \_/  |_|\___| \_/\_/ |___/ |____/ \___/ \__|                                                  
-                                              |___/                                                                                                  
-
-                                                                     \033[41m FOS- Fools of Security :)
-\033[0m
-"""
 #Default Tor port configuration
 proxyPort=9050
 ctrlPort=9051
@@ -27,8 +17,7 @@ blog = input("Enter The number of Viewers : ")
 def run():
      response = tr.get(site, headers=headers,verify=False)
 #     time.sleep(10)
-#     print(response.text)  
-     print ("["+str(i)+"]" + " Blog View Added With IP:" +  tr.get('http://ipecho.net/plain').content)
+      print(response.text)  
      tr.reset_identity()
   
 
